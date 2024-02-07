a6132fbdd88f33532d97baf9b1f4ca46e87c8860
topu <topukhan735@gmail.com>
2024-02-07 08:23:38 +0600

    used props in AppButton component to dynamically set states

---
+++ js/components/App.js
@@ -0,0 +1,12 @@
+import AppButton from "./AppButton.js"
+
+export default {
+    components: {
+        "app-button": AppButton,
+    },
+    data() {
+        return {
+            processing: true,
+        };
+    },
+};
--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,12 +1,25 @@
 export default {
     template: `
-        <button class="bg-blue-400 hover:bg-blue-600 mx-2 px-4 py-2 rounded text-white cursor-not-allowed" :disabled="processing">
+        <button 
+        :class="{
+            'mx-2 px-4 py-2 rounded text-white': true,
+            'bg-gray-400': type === 'muted',
+            'bg-blue-600': type === 'primary',
+            'bg-pink-600': type === 'secondary',
+            'is-loading': processing,
+        }" 
+        :disabled="processing">
             <slot/>
         </button>
     `,
-    data() {
-        return {
-            processing: true,
-        };
+    props: {
+        type: {
+            type: String,
+            default: "primary",
+        },
+        processing: {
+            type: Boolean,
+            default: false,
+        },
     },
 };
