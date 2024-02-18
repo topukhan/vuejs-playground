6df147142c8303015eebdc037b0e987727a19017
topu <topukhan735@gmail.com>
2024-02-18 17:34:00 +0600

    heading add

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -7,6 +7,7 @@ export default {
     },
     template: `
         <section class="space-y-6">
+            <h1 class="text-3xl font-bold">Vue JS Task Tracker</h1>
             <assignment-list v-bind:assignments="filters.inProgress" title="In Progress"></assignment-list>
             <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
 
@@ -19,8 +20,9 @@ export default {
             assignments: [
                 {name: "3rd Party Api Integration", complete: false, id: 1, tag:'personal'},
                 { name: "PHP OOP", complete: false, id: 2, tag:'personal'},
-                { name: "Vue JS initial", complete: false, id: 3, tag: 'work' },
-                { name: "Blog site WP", complete: false, id: 4, tag: 'personal'},
+                { name: "Laravel with MongoDB", complete: false, id: 3, tag: 'work' },
+                { name: "C++ Problem Solving", complete: false, id: 4, tag: 'personal'},
+                { name: "Vue.JS Task Tracker", complete: false, id: 4, tag: 'personal'},
             ],
             
         };
